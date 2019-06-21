c4ae11acdeb87c502ecf8964e834708a48cda371
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2019-06-21 17:58:51 +0700

    pull voyager-test

--- app/Http/Controllers/TCMW/NewsController.php
+++ app/Http/Controllers/TCMW/NewsController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers\TCMW;
 
 use App\Http\Controllers\Controller;
 use TCG\Voyager\Models\Post;
+use TCG\Voyager\Models\Page;
 
 class NewsController extends Controller
 {
     //
     public function news() {
         $posts = Post::all();
-        return view('tcmw/news', ['nav' => 'news'], compact('posts'));
+        $about = Page::where('slug', '=', 'about')->firstOrFail()->body;
+        return view('tcmw/news', ['nav' => 'news'], compact('posts', 'about'));
     }
 
     public function show($slug) {
         $posts = Post::all();
         $post = Post::where('slug', '=', $slug)->firstOrFail();
-	    return view('tcmw/news_single', ['nav' => 'news'], compact('posts', 'post'));
+        $about = Page::where('slug', '=', 'about')->firstOrFail()->body;
+	    return view('tcmw/news_single', ['nav' => 'news'], compact('posts', 'post', 'about'));
     }
 }
